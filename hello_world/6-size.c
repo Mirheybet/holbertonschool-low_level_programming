c4b62a4eb46ab4e99ac0d1aaee3d671a89cfc304
Mirheybet <mirheybet.hesenov@gmail.com>
2024-06-19 21:52:52 +0000

    lnx7-tst2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)", sizeof(char));
 	printf("Size of a int: %zu byte(s)", sizeof(int));
 	printf("Size of a long int: %zu byte(s)",sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)",sizeof(long long int));;
+	printf("Size of a long long int: %zu byte(s)",sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
