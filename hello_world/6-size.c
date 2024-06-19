c68851e752d14e51e60d3f42a5d2f38f5d3ba4dc
Mirheybet <mirheybet.hesenov@gmail.com>
2024-06-19 21:51:30 +0000

    lnx7-tst1

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+/*
+ * File=/holbie/chello/6th.
+ * Author=Miri.
+ */
+
+#include <stdio.h>
+
+/**
+ *main-func.
+ *
+ *Return  : 0.
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)", sizeof(char));
+	printf("Size of a int: %zu byte(s)", sizeof(int));
+	printf("Size of a long int: %zu byte(s)",sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)",sizeof(long long int));;
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
