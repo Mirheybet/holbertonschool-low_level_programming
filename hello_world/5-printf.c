4ffb21bcf444cf07b4d36cf5e090c82dbf2789ad
Mirheybet <mirheybet.hesenov@gmail.com>
2024-06-19 20:53:51 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-/**
- * main - enterpoint
- * Return: Nothing
+/*
+ * File: 5-printf.c
+ * Author: Miri
  */
 
+#include <stdio.h>
 
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: zero.
+ */
 int main(void)
 {
-        
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
 
+	return (0);
 }
