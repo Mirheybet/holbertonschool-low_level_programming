a44ef713201c5164d566aa21d0fe6ea99ea7c514
Mirheybet <mirheybet.hesenov@gmail.com>
2024-06-22 22:46:08 +0000

    lnx4-tst1

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * Return: main
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
