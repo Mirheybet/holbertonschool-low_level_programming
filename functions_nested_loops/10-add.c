901d8ec139cb509dae83fec99449db9450ead58d
Mirheybet <mirheybet.hesenov@gmail.com>
2024-06-22 21:59:55 +0000

    lnx11-tst5

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,10 +1,9 @@
 #include "main.h"
-/*
- *
- *add - main
- *@a - var 1
- *@b - var 2
- *Return: zero
+/**
+ * add - main function
+ * @a: var 1
+ * @b: var 2
+ * Return: zero
  *
  */
 int add(int a, int b)
