0d038f91746231c6c1307e0aed9a7bb9a4e05f05
Mirheybet <mirheybet.hesenov@gmail.com>
2024-06-22 15:05:29 +0000

    lnx2-tst1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - main.
+ * Return:  0.
+ */
+void print_alphabet(void)
+{
+	char herf;
+
+	for (herf = 'a'; herf <= 'z'; herf++)
+	{
+		_putchar(herf);
+	}
+	_putchar('\n');
+}
