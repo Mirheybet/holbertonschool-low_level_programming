6bc349c83eee6782e954837970cb17979451c37a
Your Name <you@example.com>
2024-06-25 13:23:23 +0000

    lnx4-tst5

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,9 +5,9 @@
  */
 void print_numbers(void)
 {
-	int eded;
+	int  eded;
 
-	for (eded = 0; eded <= 9; eded++)
-		_putchar(eded);
-	_putchar("\n");
+	for (eded = '0'; eded <= '9'; eded++)
+		_putchar(eded + '0');
+	_putchar('\n');
 }
