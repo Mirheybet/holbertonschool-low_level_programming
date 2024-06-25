73f2608c6f7fa9128e3a7b73a2b3db0b68cfd02f
Your Name <you@example.com>
2024-06-25 14:31:12 +0000

    lnx5-tst1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+/**
+ * more_numbers - main
+ * Return: zero
+ */
+void more_numbers(void)
+{
+	int eded;
+
+	for (eded = 0; eded <= 9; eded++)
+	{
+		for (eded = 0; eded <= 14; eded++)
+		{
+			_putchar(eded + '0');
+		}
+	}
+	_putchar('\n')
+
+
+}
