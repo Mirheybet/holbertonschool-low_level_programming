0c87da8586f191f3a55df7e553e023a495721272
Your Name <you@example.com>
2024-06-28 18:38:48 +0000

    lnx10-tst1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - main
+ * @dest: var 1
+ * @src: var 2
+ *
+ * Return: parametr dest
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
