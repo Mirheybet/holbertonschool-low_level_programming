614b9dac474e573152868635313415c697f6c067
Your Name <you@example.com>
2024-07-14 01:58:18 +0000

    lnx4-tst1

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - main
+ * @min: v1
+ * @max: v2
+ *
+ * Return: ptrfor_arr
+ */
+int *array_range(int min, int max)
+{
+	int *ptrfor_arr, index, size;
+
+	size = max - min + 1;
+
+	ptrfor_arr = malloc(sizeof(int) * size);
+
+	if (ptrfor_arr == NULL || min > max)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		ptrfor_arr[index] = min++;
+
+	return (ptrfor_arr);
+}
