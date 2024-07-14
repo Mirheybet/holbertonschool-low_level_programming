fdf7cb83cd9f4f38e660b8799cb9136ec6e4c0b6
Your Name <you@example.com>
2024-07-14 19:13:51 +0000

    lnx5-tst1

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * _strlen - main1
+ * @str: vp1
+ *
+ * Return: size
+ */
+int _strlen(char *str)
+{
+	int size = 0;
+
+	while (*str++)
+		size++;
+
+	return (size);
+}
+
+/**
+ * _strcopy - main2
+ * @dest: vp1
+ * @src: vp2
+ *
+ * Return: vp1
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int strcopy_index = 0;
+
+	for (strcopy = 0; src[strcopy_index]; strcopy_index++)
+		dest[strcopy_index] = src[ctrcopy_index];
+
+	dest[strcopy_index] = '\0';
+
+	return (dest);
+}
