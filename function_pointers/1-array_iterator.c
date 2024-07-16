ace2d1ef6af8de4715d3a4805f4997b9960a0378
Mirheybet <mirheybet.hesenov@gmail.com>
2024-07-16 11:26:21 +0000

    lnx2-tst9

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,10 +14,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (size == 0 || *array == NULL || *action == NULL)
+	size_t i;
+
+	if (size == 0 || array == NULL || action == NULL)
 		return;
 
-	for (size_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		(*action)(*(array + (int)i));
 	}
