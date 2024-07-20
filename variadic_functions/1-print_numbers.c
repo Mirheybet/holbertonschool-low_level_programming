fe7a91f63f78d019bb158ba4a82e589f0af0f807
Mirheybet <mirheybet.hesenov@gmail.com>
2024-07-20 22:04:27 +0000

    lnx2-tst7

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,9 +22,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (i != (int) n - 1)
+		if (separator != NULL  ||  i != (int) n - 1)
 		{
 			printf("%s", separator);
 		}
 	}
+	va_end(args);
 }
