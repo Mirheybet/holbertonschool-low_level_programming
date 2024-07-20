52d3769b9dec71b3e28b77ba67858b12108e997e
Mirheybet <mirheybet.hesenov@gmail.com>
2024-07-20 22:25:11 +0000

    lnx3-tst1

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdargs>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - main
+ * @separator: vp1
+ * @n: var2
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+
+	va_list(args);
+
+	va_start(args);
+
+	for (; i < (int)n ; i++)
+	{
+		printf("%s\n", va_arg(args, char*));
+
+		if (separator == NULL && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
