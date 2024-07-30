4b74c671d4dba81bc0550c5748e9227b8ec01bb2
Your Name <you@example.com>
2024-07-30 16:45:13 +0000

    lnx2-tst1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_dlistint - main
+ * dlistint_len - main
  * @h: Clone of dblylink
  * Return: count of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t countnode = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		countnode++;
 		h = h->next;
 	}
