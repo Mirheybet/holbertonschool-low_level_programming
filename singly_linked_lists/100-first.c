91ab1b2d23e8d4fa7a3a59a71a8935a10627a164
Your Name <you@example.com>
2024-07-30 11:26:31 +0000

    lnx101-tst2

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@
 
 void __attribute__((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
 
