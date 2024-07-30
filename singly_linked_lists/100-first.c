a8f7a349b01cfd3fe589d145b091f34d8c93e9cc
Your Name <you@example.com>
2024-07-30 11:25:47 +0000

    lnx101-tst1

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * __attribute__ - main
+ * @constructor: var1
+ */
+
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
+
