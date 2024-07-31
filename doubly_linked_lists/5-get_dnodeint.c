8fd8caa54dd57e8600e0e04399d612189e44b476
Mirheybet <mirheybet.hesenov@gmail.com>
2024-07-31 20:45:29 +0000

    lnx6-tst1

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * get_dnodeint_at_index - main
+ * @head: clone dblylinkedlist
+ * @index: nth index
+ * Return: head
+ */
+
+
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int nth = index;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		for (; nth > 0; nth--)
+		{
+			head = head->next;
+		}
+		return (head);
+	}
+
+}
