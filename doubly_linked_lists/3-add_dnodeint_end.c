62edbc1204eba5fbefd8b040d9942c0edeb38c05
Mirheybet <mirheybet.hesenov@gmail.com>
2024-07-31 19:57:10 +0000

    lnx4-tst3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,23 +13,32 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *backnode = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *th1node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *th2node;
 
-	if (backnode == NULL)
+	th2node = *head;
+	if (th1node == NULL)
 	{
 		return (NULL);
 	}
 
-	backnode->n = n;
-	backnode->prev = *head;
-	backnode->next = NULL;
+	th1node->n = n;
+	th1node->next = NULL;
 
 	if (*head == NULL)
 	{
-		(*head)->next = backnode;
+		th1node->prev = NULL;
+		*head = th1node;
+		return (th1node);
 	}
 
-	*head = backnode;
-	return (backnode);
+	while (th2node->next != NULL)
+	{
+		th2node = th2node->next;
+	}
 
+	th2node->next = th1node;
+	th1node->prev = th2node;
+
+	return (th1node);
 }
