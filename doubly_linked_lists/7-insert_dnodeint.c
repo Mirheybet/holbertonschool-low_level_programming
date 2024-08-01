8f388ab0b7f6f8c2b169200294e9b977fa2af80e
Mirheybet <mirheybet.hesenov@gmail.com>
2024-08-01 07:43:35 +0000

    lnx8-tst1

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - main
+ * @h: clone of dblylink
+ * @idx: for indexes
+ * @n: dblylink data
+ * Return: 1th node
+ *
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *th1 = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *th2 = *h;
+
+	if (th1 == NULL)
+	{
+		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+
+	for (; idx > 1; idx--)
+	{
+		th2 = th2->next;
+		if (th2 == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	if (th2->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
+	th1->n = n;
+	th1->next = th2->next;
+	th1->prev = th2;
+	th2->next->prev = th1;
+	th2->next = th1;
+
+	return (th1);
+
+
+}
