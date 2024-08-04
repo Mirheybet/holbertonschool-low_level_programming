0dd748397845a34c47ebd7b06479fa8a0802a65a
Your Name <you@example.com>
2024-08-04 13:52:20 +0000

    lnx3-tst1

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int indexcomp = sizeof(n) * 8;
+
+	if (indexcomp == index || index > indexcomp)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return ((n >> index) & 1);
+	}
+
+}
